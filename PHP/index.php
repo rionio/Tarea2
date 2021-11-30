8b373c72023ae7266d7cdce4056c68f3ee5f313e
rionio <j.abarca.beretta@gmail.com>
2021-11-29 21:56:00 -0300

    Third commit

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,25 @@
-<?php echo "Hola weones";
-
+<?php 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio Web</title>
+
+    <link rel="stylesheet" href="./CSS/bootstrap.min.css" />
+</head>
+<body>
+        <nav class="navbar navbar-lg navbar-dark bg-primary">
+            <ul class="nav navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nav 1 </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nav 2</a>
+                </li>
+            </ul>
+        </nav>
+</body>
+</html>
--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Links de referencia
 
 * [HTML](https://www.w3schools.com/html/html_intro.asp)
 * [PHP](https://www.w3schools.com/php/default.asp)
-* [Boostrap4](https://www.w3schools.com/bootstrap4/default.asp)
+* [Bootsrap4](https://www.w3schools.com/bootstrap4/default.asp)
 * [Markdown](https://markdown.es/sintaxis-markdown/)
