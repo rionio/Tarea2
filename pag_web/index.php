bdf8c951eb4c3769e6caa934acc8534ef0a79ec9
Richarduu <ricardo.diazca@usm.cl>
2021-11-29 23:40:02 -0300

    file principal

---
+++ pag_web/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea2</title>
+
+    <link rel="stylesheet" href="./css/bootstrap.min.css"/>
+</head>
+<body>
+
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pagina Principal</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Carrito</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Iniciar sesion</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="jumbotron">
+                <h1 class="display-3">Tienda</h1>
+                <p class="lead">Bienvenido</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                </p>
+            </div>
+
+
+
+
+            
+        </div>
+    </div>
+</body>
+</html>
