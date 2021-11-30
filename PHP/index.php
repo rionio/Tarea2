db34251799b78f722a3e2bf1dfc3ac1a3448491c
Richarduu <ricardo.diazca@usm.cl>
2021-11-30 16:25:30 -0300

    arreglos

--- PHP/index.php
+++ PHP/index.php
@@ -1,25 +1,46 @@
-<?php 
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sitio Web</title>
+    <title>Tarea2</title>
 
-    <link rel="stylesheet" href="./CSS/bootstrap.min.css" />
+    <link rel="stylesheet" href="./css/bootstrap.min.css"/>
 </head>
 <body>
-        <nav class="navbar navbar-lg navbar-dark bg-primary">
-            <ul class="nav navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Nav 1 </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Nav 2</a>
-                </li>
-            </ul>
-        </nav>
+
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pagina Principal</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Carrito</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Iniciar sesion</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="jumbotron">
+                <h1 class="display-3">Tienda</h1>
+                <p class="lead">Bienvenido</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                </p>
+            </div>
+
+
+
+
+            
+        </div>
+    </div>
 </body>
 </html>
