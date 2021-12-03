348a1304a3eb63f0802b61d3ac72ad87c7c18b7b
Richardduu <ricardo.diazca@usm.cl>
2021-12-03 01:08:11 -0300

    upload

---
+++ PHP/USERproductos.php
@@ -0,0 +1,54 @@
+<?php include("template/USERhead.php"); ?>
+
+<div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-3" style="text-align: center;">Productos</h1>
+        </div>
+    </div>
+
+    <div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="holder.js/100x180/" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+    </div>
+    </div>
+
+
+    <div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="holder.js/100x180/" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+    </div>
+    </div>    
+    
+    
+    <div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="holder.js/100x180/" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+    </div>
+    </div>    
+    
+    
+    <div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="holder.js/100x180/" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+    </div>
+    </div>
+
+
+
+<?php include("template/pie.php"); ?>
