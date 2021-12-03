5b21753c32c94d404296983d3e7f978dd5c4fa51
rionio <j.abarca.beretta@gmail.com>
2021-12-03 01:47:50 -0300

    Changed name

--- PHP/ProductsShop.php
+++ PHP/ProductsShop.php
@@ -6,16 +6,24 @@
         </div>
     </div>
 
-    <div class="col-md-3">
-    <div class="card">
-        <img class="card-img-top" src="holder.js/100x180/" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <p class="card-text">Text</p>
+    <div class="productos">
+        <div class="row">
+            <div class = "col-xs-10 col-sm-6 col-md-4">
+                <div class="card">
+                    <img src="./Image" alt="Imagen de">
+                    <h3>Nombre</h3>
+                    <p>precio</p>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="holder.js/100x180/" alt="">
+                        <div class="card-body">
+                            <h4 class="card-title">Title</h4>
+                            <p class="card-text">Text</p>
+                        </div>
+                </div>
+            </div>
         </div>
     </div>
-    </div>
-
 
     <div class="col-md-3">
     <div class="card">
