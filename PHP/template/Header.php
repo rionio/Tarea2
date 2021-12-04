2ae8e04fd933ec0fd02795e828b8cfe8bcc46b60
rionio <j.abarca.beretta@gmail.com>
2021-12-03 22:47:09 -0300

    Updated files for a more dynamic web

--- PHP/template/Header.php
+++ PHP/template/Header.php
@@ -10,14 +10,15 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 </head>
 <body>
-
+  <?php
+      require_once("User.isLogin.php");
+  ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="UserIndex.php">SansApp</a>
+    <a class="navbar-brand" href="index.php">SansApp</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarColor01">
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
@@ -41,29 +42,42 @@
             <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
         </form>
       </ul>
-      <ul class="navbar-nav mr-auto">
-        <img src="https://lh3.googleusercontent.com/proxy/7GkW6bKqAnbzN4Yeg_4TkF6ItfqocMD1kMFUZrf7n5AtWuY9LieNOQjFO2nU93zVCaOQw1kJoeLqyi90frAX48xuQX7z7cycL-aYRu0vEmk1LhDIbGZZ0nAt6XJ_pu2xrv4koGo=w1200-h630-p-k-no-nu" width="50">
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
         <?php
-          //  session_start();
-          //  $user=$_SESSION['nombre_usuario'];
-          //  echo "<a class='nav-link'> $user</a>";
-          ?>test
-        </a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="User.profile.php">Perfil</a>
-          <a class="dropdown-item" href="#">Carrito</a>
-          <a class="dropdown-item" href="#">Historial Compra</a>
-          <a class="dropdown-item" href="#">Historial Venta</a>
-          <a class="dropdown-item" href="UserSell.php">Vender producto</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="User.close.php">Salir</a>
-        </div>
-        </li>
-      </ul>
-<!--quitale los comentarios y los tests, sabes que no me funca la base de datos XD-->
-
+          if($status){
+            echo'
+                <ul class="navbar-nav mr-auto">
+                  <img src="https://lh3.googleusercontent.com/proxy/7GkW6bKqAnbzN4Yeg_4TkF6ItfqocMD1kMFUZrf7n5AtWuY9LieNOQjFO2nU93zVCaOQw1kJoeLqyi90frAX48xuQX7z7cycL-aYRu0vEmk1LhDIbGZZ0nAt6XJ_pu2xrv4koGo=w1200-h630-p-k-no-nu" width="50">
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                    '
+                    . $user .
+                    '
+                    </a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="UserProfile.php">Perfil</a>
+                      <a class="dropdown-item" href="#">Carrito</a>
+                      <a class="dropdown-item" href="#">Historial Compra</a>
+                      <a class="dropdown-item" href="#">Historial Venta</a>
+                      <a class="dropdown-item" href="UserSell.php">Vender producto</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="User.close.php">Salir</a>
+                    </div>
+                  </li>
+                </ul>
+            ';
+          }else{
+            echo('
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="UserSignIn.php">Iniciar Sesion</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="UserSignUp.php">Registrarse</a>
+                </li>
+              </ul>
+            ');
+          }
+        ?>
     </div>
   </div>
 </nav>
