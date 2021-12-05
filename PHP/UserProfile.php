6832ece0bfcbb743bdb08a2e21acf3002ea0a1da
Richardduu <ricardo.diazca@usm.cl>
2021-12-05 19:41:50 -0300

    upgrade para el critico destructivo

--- PHP/UserProfile.php
+++ PHP/UserProfile.php
@@ -7,8 +7,12 @@
 #    exit;
 #    }
 ?> 
+<div class="global-container" style="	height:100%; display: flex;align-items: center;justify-content: center;">
+	<div class="card login-form" style="	width:100%;margin:20px;">
+    <div class="card-body" style="color: steelblue;background-color: whitesmoke;">
+
 <div class="jumbotron" style="width: 80%;">
-    <h1 class="display-1">Perfil</h1>
+    <h2 class="display-1">Perfil</h2>
     <a href="UserEditProfile.php" class="lead">Editar perfil</a>
     <p></p>
     <div class="list-group" style="width: 60%; float:left">
@@ -16,22 +20,30 @@
     <h4 class="list-group-item list-group-item-action active">Correo: <?php echo $user;?></h4> 
     <h4 class="list-group-item list-group-item-action active">Cant. profuctos vendidos: <?php echo 'test'; //aqui va la cant venta?></h4>
     <h4 class="list-group-item list-group-item-action active">Cant. profuctos comprados: <?php echo 'test'; //aqui va cant compra?></h4> 
-
     </div>
+    
+    
     <div style="width: 20%; float:right">
         <img src="https://lh3.googleusercontent.com/proxy/7GkW6bKqAnbzN4Yeg_4TkF6ItfqocMD1kMFUZrf7n5AtWuY9LieNOQjFO2nU93zVCaOQw1kJoeLqyi90frAX48xuQX7z7cycL-aYRu0vEmk1LhDIbGZZ0nAt6XJ_pu2xrv4koGo=w1200-h630-p-k-no-nu"
-        width="300">
-</div>
-
-</div>
-
-
-
-
-
-    <p class="lead">
+        width="250">
+        <p></p>
+        <!-- <p class="lead"> -->
     <input type="submit" value="Borrar Perfil" class="btn btn-outline-danger" onclick="return confirm('estas seguro?')"/>
-    </p>
+    <!-- </p> -->
+</div>
+
+</div>
+
+    </div>
+    
+    </div>
+</div>
+
+
+
+
+
+
 
 
 
