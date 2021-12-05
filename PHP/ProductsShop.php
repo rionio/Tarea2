d50a33b99933d6349869efe860f9108e32761925
Richardduu <ricardo.diazca@usm.cl>
2021-12-04 21:47:49 -0300

    test update

--- PHP/ProductsShop.php
+++ PHP/ProductsShop.php
@@ -1,60 +1,31 @@
 <?php include("template/Header.php"); ?>
+<p></p>
+<table class="table table-hover"> <!--cabecera de la tabla-->
+<tr class="table-dark">
+    <th>Producto</th>
+    <th>Precio</th>
+    <th>Stock</th>
+    <th></th>
+</tr>
+<?php
 
-    <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-            <h1 class="display-3" style="text-align: center;">Productos</h1>
-        </div>
-    </div>
+$sql="select * from producto";
+$result=pg_query($status,$sql);//aqui es la creacion de la lista
+while ($show=pg_fetch_array($result)){//recursividad de la lista
 
-    <div class="productos">
-        <div class="row">
-            <div class = "col-xs-10 col-sm-6 col-md-4">
-                <div class="card">
-                    <img src="./Image" alt="Imagen de">
-                    <h3>Nombre</h3>
-                    <p>precio</p>
-                </div>
-                <div class="card">
-                    <img class="card-img-top" src="holder.js/100x180/" alt="">
-                        <div class="card-body">
-                            <h4 class="card-title">Title</h4>
-                            <p class="card-text">Text</p>
-                        </div>
-                </div>
-            </div>
-        </div>
-    </div>
+?>
+<tr>
+    <td><?php echo "$show['Nombre']" ?></td>
+    <td><?php echo "$show['Precio']" ?></td>
+    <td><?php echo "$show['Unidades disponibles']" ?></td>
+    <td><a href="#">Test</a></td>
 
-    <div class="col-md-3">
-    <div class="card">
-        <img class="card-img-top" src="holder.js/100x180/" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <p class="card-text">Text</p>
-        </div>
-    </div>
-    </div>    
-    
-    
-    <div class="col-md-3">
-    <div class="card">
-        <img class="card-img-top" src="holder.js/100x180/" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <p class="card-text">Text</p>
-        </div>
-    </div>
-    </div>    
-    
-    
-    <div class="col-md-3">
-    <div class="card">
-        <img class="card-img-top" src="holder.js/100x180/" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <p class="card-text">Text</p>
-        </div>
-    </div>
-    </div>
+</tr>
+<?php
+}
+?>
+
+</table>
+
 
 <?php include("template/pie.php"); ?>
