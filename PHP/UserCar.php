d7931858788599bf992113bd3513d5365d658ca7
Richardduu <ricardo.diazca@usm.cl>
2021-12-06 18:36:46 -0300

    upgrade and upload

---
+++ PHP/UserCar.php
@@ -0,0 +1,22 @@
+<?php include("template/Header.php"); ?>
+<div class="global-container" style="	height:100%; display: flex;align-items: center;justify-content: center;">
+<div class="card login-form" style="	width:100%;margin:20px;">
+<div class="card-body" style="color: steelblue;background-color: white;">
+
+<h1>Carro</h1>
+<table class="table table-hover">
+<tr class="table-primary">
+    <th>Producto</th>
+    <th>Precio</th>
+    <th>ID</th>
+    <th></th>
+</tr>
+<!-- implementar vista a carrito -->
+</table>
+<div style="width: 60%; float:right">
+<button type="button" class="btn btn-outline-success" style="width: 40%;">Comprar todo</button>
+</div>
+</div>
+</div>
+</div>
+<?php include("template/pie.php"); ?>
--- PHP/UserSell.php
+++ PHP/UserSell.php
@@ -1,5 +1,8 @@
 <?php include("template/Header.php"); ?>
     <p></p>
+<div class="global-container" style="height:100%; display: flex;align-items: center;justify-content: center;">
+<div class="card login-form" style="	width:100%;margin:20px;">
+<div class="card-body" style="color: steelblue;background-color: whitesmoke;">
 <div class="jumbotron">
     <h1 class="display-3">Vender Producto</h1>
     <p class="lead">Ingrese los datos del producto a vender</p>
@@ -12,7 +15,6 @@
     <div class="form-group">
     <label class="col-form-label col-form-label-lg mt-4" for="input">Nombre producto</label>
     <input class="form-control form-control-lg" type="text" id="inputLarge">
-    <p></p>
     <label for="exampleTextarea" class="form-label mt-4">Descripcion</label>
     <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
     </div>
@@ -23,7 +25,6 @@
     <div class="form-group">
     <label class="col-form-label col-form-label-lg mt-4" for="input">Precio</label>
     <input class="form-control form-control-lg" type="text" id="inputLarge" placeholder="$">
-    <p></p>
     <div class="form-group">
       <label for="exampleSelect1" class="form-label mt-4">Caregoria</label>
       <select class="form-select" id="exampleSelect1">
@@ -34,7 +35,7 @@
         <option>drugs</option>
       </select>
     </div>
-    <p></p>
+
     <div class="form-group col-sm-3">
     <label class="col-form-label col-form-label-sm mt-2" for="inputSmall">Cantidad</label>
     <input class="form-control form-control-sm" type="text" id="inputSmall">
@@ -42,16 +43,22 @@
 
 </td>
 <td>
-    <div style="text-align: right;">
+    <div style="width: 50%; float:center">
                 <img src="https://c.tenor.com/YvaE5INKypcAAAAC/money-cash.gif"
-                width="400">
+                width="300"  >
+                <p></p>
+                    <button type="button" class="btn btn-outline-primary">Primary</button>
+
                 </div>
     </td>
 </tr>
 
 </table>
+</div>
 
+</div>
 
+</div>
 </div>
 
 
