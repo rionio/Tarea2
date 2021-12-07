f8df9ed67db03e4c15038f5a258a0165c50bb2cf
Richardduu <ricardo.diazca@usm.cl>
2021-12-07 15:14:45 -0300

    Upload View

---
+++ PHP/View.php
@@ -0,0 +1,53 @@
+<?php include("template/Header.php"); ?>
+
+<div class="global-container" style="	height:100%; display: flex;align-items: center;justify-content: center;" backg>
+    <div class="card login-form" style="	width:100%;margin:20px;">
+        <div class="card-body" style="color: steelblue;background-color: whitesmoke;">
+            <div class="container">
+                <div class="row" style="width: 30%; float:left">
+                    <img src="https://somoskudasai.com/wp-content/uploads/2021/07/A2-9.jpg" alt="xd"
+                    width="400">
+                </div>
+            </div>
+            <div class="container">
+                    <div class="row">
+                        <h1><strong>Info. Producto</strong></h1>
+                        <h1>Nombre: test</h1>
+                        <div class="container">
+                            <div class="row">
+                            <h5>Calificacion: x ✰ <a href="#">Calificar</a></h5>
+                            </div>
+                        </div>
+                        <div class="container">
+                            <div class="row" style="width: 40%; float:right">
+                                <h3>Precio:$</h3>
+                                <h5>Publicado por: test</h5>
+                            </div>
+                            <p></p>
+                            <button type="button" class="btn btn-outline-warning">Añadir al carro</button>
+                        </div>
+
+                    </div>
+            </div>
+
+        </div>
+        <div class="container"><p></p>
+        <h3><abbr title="attribute">Comentarios</abbr></h3>
+        <div class="row">
+            <table>
+            <tr class="table-primary">
+                <th width="150">Usuario</th>
+                <th>Comentario</th>
+            </tr>  
+            <tr>
+                <th>test</th>
+                <th>test2</th>
+            </tr> 
+            </table>
+        </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include("template/pie.php"); ?>
